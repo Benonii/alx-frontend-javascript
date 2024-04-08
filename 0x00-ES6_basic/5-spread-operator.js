6e3398fe9101bde67d0e85a3f93a63b4149e049f
BENONI ESCKINDER <you@example.com>
2024-04-09 02:15:56 +0300

    Actually made function one line long

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,2 @@
 export default function concatArrays(array1, array2, string) {
-  return ([...array1, ...array2, ...Array.from(string)]);
-}
+  return ([...array1, ...array2, ...Array.from(string)]); }
