52a5b4996822713ae1fb1a67df9caa233093e18f
BENONI ESCKINDER <you@example.com>
2024-04-09 01:15:42 +0300

    ESlint fix

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,6 @@
-export default function concatArrays(array1, array2, string) {
+export default function concatArrays (array1, array2, string) {
   return ([...array1,
     ...array2,
-    ...Array.from(string),
+    ...Array.from(string)
   ]);
 }
