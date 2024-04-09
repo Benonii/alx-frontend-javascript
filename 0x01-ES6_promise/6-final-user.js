bc6371aa162c99335de90e8c989f569e4ec522de
BENONI ESCKINDER <you@example.com>
2024-04-10 02:30:49 +0300

    Fixed then() function to have a return statement

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,17 +5,8 @@ export default function handleProfileSignUp(firstName, lastName, fileName) {
   const allPromises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
   const finalUser = Promise.allSettled(allPromises)
-    .then((objArray) => [
-      {
-        status: objArray[0].status,
-        value: objArray[0].value,
-      },
-      {
-        status: objArray[1].status,
-        value: objArray[1].reason,
-      },
-    ]).catch(() => {
-      // Nothing
+    .then((objArray) => objArray).catch(() => {
+      Error();
     });
 
   return finalUser;
