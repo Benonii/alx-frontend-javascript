13f5dc6b0f0e784c4b5fea6dfb4f56865be39c96
BENONI ESCKINDER <you@example.com>
2024-04-10 01:12:05 +0300

    Replacing .any() with .allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,19 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   const allPromises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  const finalUser = Promise.any(allPromises)
-    .then((resolvedValues) => [
-      {
-        status: resolvedValues[0].status,
-        value: `${resolvedValues[0].firstName} ${resolvedValues[0].lastName}`,
-      },
-      {
-        status: resolvedValues[1].status,
-        value: resolvedValues[1],
-      },
-    ]).catch(() => {
-      // Nothing to see here folks
-    });
+  const finalUser = Promise.allSettled(allPromises);
 
   return finalUser;
 }
