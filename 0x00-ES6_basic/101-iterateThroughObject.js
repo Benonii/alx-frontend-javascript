c973c2dc9f1db9b5e75ed7170482b7e86a8da94f
BENONI ESCKINDER <you@example.com>
2024-04-09 05:15:00 +0300

    ESlint fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
   let employeeNames = '';
 
-  for (const { value : employeeName } of reportWithIterator) {
-    employeeNames += employeeName + " | ";
+  for (const { value: employeeName } of reportWithIterator) {
+    employeeNames += `${employeeName} | `;
   }
 
   employeeNames = employeeNames.slice(0, -2);
 
   return employeeNames;
-};
+}
