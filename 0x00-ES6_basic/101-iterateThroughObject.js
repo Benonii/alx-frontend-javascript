ab79502a7eb4664c68592e92e6f9e81a7eb48fed
BENONI ESCKINDER <you@example.com>
2024-04-09 05:30:54 +0300

    final attemt... trying to access elements as objects instead of strings

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
   let employeeNames = '';
 
-  for (const { value: employeeName } of reportWithIterator) {
-    employeeNames += `${employeeName} | `;
+  for (const item of reportWithIterator) {
+    employeeNames += `${item.value} | `;
   }
 
   employeeNames = employeeNames.slice(0, -2);
