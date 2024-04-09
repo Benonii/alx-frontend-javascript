47631d38459d33ea98f8d44043496cc5b9d70849
BENONI ESCKINDER <you@example.com>
2024-04-10 00:43:40 +0300

    Changed catch expression

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,5 +3,7 @@ export default function handleResponseFromAPI(promise) {
     console.log('Got a response from the API');
     return ('success');
   }, 200);
-  promise.catch(() => ({}));
+  promise.catch(() => {
+    // Nothing
+  });
 }
