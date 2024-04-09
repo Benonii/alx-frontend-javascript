9ee850c08107d47b6fdddfe18409f154033fb7b6
BENONI ESCKINDER <you@example.com>
2024-04-10 02:12:20 +0300

    Adding a return statement inside then()

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignUp() {
   const result = Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      const profileString = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
+      console.log(profileString);
+      return profileString;
     }).catch(() => {
       console.log('Signup system offline');
     });
