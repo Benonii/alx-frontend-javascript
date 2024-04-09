17f96460bf8104d8e819901ac7bcdad778fadec3
BENONI ESCKINDER <you@example.com>
2024-04-10 00:40:47 +0300

    Fixed syntax error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
-    return('success',), 200};
-  });
+    return ('success');
+  }, 200);
   promise.catch(() => ({}));
 }
