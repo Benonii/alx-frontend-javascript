6286aba3bb262f52583b0cedbdf7aabcd42d21f1
BENONI ESCKINDER <you@example.com>
2024-04-10 02:08:22 +0300

    Adding a return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignUp() {
-  Promise.all([uploadPhoto(), createUser()])
+  const result = Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
     }).catch(() => {
       console.log('Signup system offline');
     });
+  return result;
 }
