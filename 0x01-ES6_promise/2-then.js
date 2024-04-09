2f6a04a1dd37a2122e5877a9c9797158314a862d
BENONI ESCKINDER <you@example.com>
2024-04-10 00:36:15 +0300

    Typo fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    console.log('Got a response form the API');
+    console.log('Got a response from the API');
     return {
       status: 200,
       body: 'success',
