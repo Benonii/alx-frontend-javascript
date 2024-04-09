7c4f069929a05691456b6c6b5c13708f398b3c1b
BENONI ESCKINDER <you@example.com>
2024-04-10 01:24:02 +0300

    Making reason => value

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,17 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   const allPromises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  const finalUser = Promise.allSettled(allPromises);
+  const finalUser = Promise.allSettled(allPromises)
+    .then((objArray) => [
+      {
+        status: objArray[0].status,
+        value: objArray[0].value,
+      },
+      {
+        status: objArray[1].status,
+        value: objArray[1].reason,
+      },
+    ]);
 
   return finalUser;
 }
