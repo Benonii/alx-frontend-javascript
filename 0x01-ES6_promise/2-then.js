9b9d8c00c7bfd13d0fb50ec590be0e371047c22d
BENONI ESCKINDER <you@example.com>
2024-04-10 01:32:03 +0300

    Added a return value

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  const result = promise.then(() => {
     console.log('Got a response from the API');
-    return ('success');
-  }, 200);
+    return {
+      status: 200,
+      body: 'success',
+    };
+  });
   promise.catch(() => {
     console.log('Got a response from the API');
   });
+
+  return result;
 }
