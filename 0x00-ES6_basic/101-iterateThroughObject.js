a23c17dbd3fac9f7845ad8251a00af269345f664
BENONI ESCKINDER <you@example.com>
2024-04-11 02:58:05 +0300

    Attempting different approaches

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = '';
+  let employeeNames = [];
   const iterator = reportWithIterator[Symbol.iterator]();
   let result = iterator.next();
 
   while (!result.done) {
-    employeeNames += `${result.value}`;
+    employeeNames.push(`${result.value} | `);
     result = iterator.next();
   }
 
