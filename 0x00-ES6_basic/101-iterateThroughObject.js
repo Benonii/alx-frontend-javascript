c63448e796ec46958032cae4bea1588697836124
BENONI ESCKINDER <you@example.com>
2024-04-11 03:07:43 +0300

    Turns out the issue was the iterator and not the iteration all along

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,14 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = [];
+  let employeeNames = '';
   const iterator = reportWithIterator[Symbol.iterator]();
   let result = iterator.next();
 
   while (!result.done) {
-    employeeNames.push(`${result.value} | `);
+    employeeNames += `${result.value} | `;
     result = iterator.next();
   }
 
-  employeeNames = employeeNames.slice(0, -2);
+  employeeNames = employeeNames.slice(0, -3);
 
   return employeeNames;
 }
