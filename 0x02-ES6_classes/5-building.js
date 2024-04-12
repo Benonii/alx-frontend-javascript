b65776f937923ad95b3e6b9997b9d5b812972516
BENONI ESCKINDER <you@example.com>
2024-04-12 05:10:39 +0300

    ESlint fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -7,7 +7,7 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
+  static evacuationWarningMessage() {
     throw new Error("Class extending Building must override evacuationWarningMessage'");
   }
 }
