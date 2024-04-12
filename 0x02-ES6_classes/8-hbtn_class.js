d2e52ddd35a3837322c5abfe16b3e173bd7cc68f
BENONI ESCKINDER <you@example.com>
2024-04-12 03:43:25 +0300

    Task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    } if (hint === 'number') {
+      return this._size;
+    }
+    throw new TypeError('Unsupported type');
+  }
+}
