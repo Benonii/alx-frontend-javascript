1ffd8a72d61f8b1573c18a4be734cb2291e6d59c
BENONI ESCKINDER <you@example.com>
2024-04-12 18:38:43 +0300

    Making sure error is thrown

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    Building.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -8,6 +9,8 @@ export default class Building {
   }
 
   static evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage'");
+    setTimeout(() => {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }, 100); // Delay for 2000 milliseconds (2 seconds)setTimeout(()
   }
 }
