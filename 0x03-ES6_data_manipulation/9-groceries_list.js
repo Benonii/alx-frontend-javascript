b00233f16d35eafa3e72be6c88bd4dacd7a5c16c
BENONI ESCKINDER <you@example.com>
2024-04-16 02:58:49 +0300

    Spelling fix

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,7 +2,7 @@ export default function groceriesList() {
   const groceries = new Map();
 
   groceries.set('Apples', 10);
-  groceries.set('Tomatos', 10);
+  groceries.set('Tomatoes', 10);
   groceries.set('Pasta', 1);
   groceries.set('Rice', 1);
   groceries.set('Banana', 5);
