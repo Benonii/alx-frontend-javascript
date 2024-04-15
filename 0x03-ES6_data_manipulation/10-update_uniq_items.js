242bdca81bd07fd5d827b5bf057d36a603b66ba3
BENONI ESCKINDER <you@example.com>
2024-04-16 02:52:42 +0300

    Added error handling

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,12 @@
 export default function updateUniqueItems(groceries) {
   for (const [key, value] of groceries.entries()) {
-    if (value === 1) {
-      groceries.set(key, 100);
-    }
+    try {
+      if (value === 1) {
+        groceries.set(key, 100);
+      }
+    } catch(e) {
+      throw new Error('Cannot process');
+    };
   }
   return groceries;
 }
