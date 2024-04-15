e286f1a98ffdebb269ce3ff3d64ee5c7ba3649a3
BENONI ESCKINDER <you@example.com>
2024-04-16 02:43:37 +0300

    Attempting different approaches

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let cleanString = '';
   set.forEach((element) => {
     if (startString !== '' && element.startsWith(startString)) {
-      cleanString += `${element.slice([startString.length])}-`;
+      cleanString += `${element.slice(startString.length)}-`;
     }
   });
   cleanString = cleanString.slice(0, -1);
