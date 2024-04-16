cddf26a8272ce634fb973535e33ab463ef52e37d
BENONI ESCKINDER <you@example.com>
2024-04-16 03:12:56 +0300

    Attempting different approaches..3

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,11 @@
 export const weakMap = new WeakMap();
 export default function queryApi(endpoint) {
   let count = 0;
-  count =+ 1;
+  count += 1;
 
   weakMap.set(JSON.stringify(endpoint), count);
 
   if (count >= 5) {
     throw new Error('Endpoint load is high');
-  };
+  }
 }
