565fe366b3a1846c954b9ff67003b3c6e103d9d7
BENONI ESCKINDER <you@example.com>
2024-04-16 03:05:57 +0300

    Specifying map not Map error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,13 @@
 export default function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, value] of groceries.entries()) {
-    try {
-      if (value === 1) {
-        groceries.set(key, 100);
-      }
-    } catch (e) {
-      throw new Error('Cannot process');
+    if (value === 1) {
+      groceries.set(key, 100);
     }
   }
+
   return groceries;
 }
